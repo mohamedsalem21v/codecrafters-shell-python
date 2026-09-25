f11e9ce29d2e58aa58297d23230686923fb4b696
salem <mohamedsalem462006@gmail.com>
2026-09-25 11:34:37 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,16 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         pass
         command = input()
         if command == "exit":
             break
-        print(f"{command}: command not found")
+        elif command.startswith("echo "):
+            print(command[5:])
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
